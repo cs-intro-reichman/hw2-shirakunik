52ead041c5d2a92d7a9917fd0211d858d4ed20c3
Shira Kunik <shirakunik@Mac.lan>
2025-11-13 00:31:36 +0200

    completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,25 +1,25 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // input a string and int number 
-             String name = args [0];
-             int n = Integer.parseInt(args [1]); 
-             name = name.toUpperCase ();
-
-             // make the loops
-         for ( int i = 0; i<name.length(); i++) {
-             char ch = name.charAt(i);
-         if ( "EFHILMNORSX".indexOf(ch) >=0 ) {
-                  System.out.println( "Give me an " + ch + ": " + ch + "!");
-           } else { 
-                System.out.println( "Give me a " + ch + ": " + ch + "!");
-           } 
-        } 
-         System.out.println("What does that spell?");
-
-         for (int j = 0; j<n; j++){
-                System.out.println( name + "!!!" );
-         }
-           }
+            String word = args[0];
+            int times = Integer.parseInt(args[1]);
+    
+            String anSet = "AEFHILMNORSX";
+    
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                char upper = Character.toUpperCase(c);
+    
+                String article = (anSet.indexOf(upper) >= 0) ? "an" : "a";
+                String pad = article.equals("a") ? "  " : " "; 
+    
+                System.out.println("Give me " + article + pad + upper + ": " + upper + "!");
+            }
+    
+            System.out.println("What does that spell?");
+            String shout = word.toUpperCase();
+            for (int i = 0; i < times; i++) {
+                System.out.println(shout + "!!!");
+            }
         }
-        
+    }
--- Collatz.java
+++ Collatz.java
@@ -1,45 +1,45 @@
-// Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-      // input N and mode
-		int N = Integer.parseInt(args[0]);  
-        String mode = args[1];               // "v" / "c"
-        boolean verbose = mode.equals("v");  
+	public static void main(String[] args) {
+			int limit = Integer.parseInt(args[0]); 
+			String mode = args[1];                
 
-        // seed n-1 
-        for (int seed = 1; seed <= N; seed++) {
+			for (int seed = 1; seed <= limit; seed++) {
+					int n = seed;
 
-            long n = seed;   
-            int terms = 1;   // counrter for the numbers 
+					
+					int printed = 0;
+					if (mode.equals("v")) {
+							System.out.print(seed + " ");
+							printed = 1; 
+					}
 
-            if (verbose) {
-                System.out.print(n);  
-            }
+					if (seed == 1) {
+							
+							if (mode.equals("v")) {
+									System.out.print(4 + " ");
+									System.out.print(2 + " ");
+									System.out.print(1 + " ");
+							}
+							printed = mode.equals("v") ? 4 : 0;
+					} else {
+							while (n != 1) {
+									if (n % 2 == 0) {
+											n = n / 2;
+									} else {
+											n = 3 * n + 1;
+									}
+									if (mode.equals("v")) {
+											System.out.print(n + " ");
+											printed++;
+									}
+							}
+					}
 
-            // loop Collatz 
-            while (n != 1) {
-                if (n % 2 == 0) {    
-                    n = n / 2;
-                } else {              
-                    n = 3 * n + 1;
-                }
+					if (mode.equals("v")) {
+							System.out.println("(" + printed + ")");
+					}
+			}
 
-                terms++; 
-
-                // if verbose
-                if (verbose) {
-                    System.out.print(" " + n);
-                }
-            }
-
-  
-            if (verbose) {
-                System.out.println(" (" + terms + ")"); 
-            }
-        }
-
-        // finish sentence
-        System.out.println();
-        System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
-    }
+			System.out.println("Every one of the first " + limit + " hailstone sequences reached 1.");
+	}
 }
